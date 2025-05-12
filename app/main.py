e8dc2281c3c20843e03941fefff8b7537683e70e
Dzvin100 <ay777888@ukr.net>
2025-05-13 00:30:26 +0300

    pull request with corrected errors

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 class Person:
     people = {}
 
-    def __init__(self, name: list, age: int ) -> None:
+    def __init__(self, name: list, age: int) -> None:
         self.name = name
         self.age = age
-        Person.people[name]= self
+        Person.people[name] = self
+
 
 def create_person_list(people: list) -> list:
     person_list = [Person(person["name"], person["age"]) for person
